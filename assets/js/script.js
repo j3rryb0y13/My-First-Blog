f49c1f7eb85f6edd468cc6232116f4134a105b95
Jeremy Koehler <Jeremykoehler18@gmail.com>
2024-03-31 00:44:22 -0400

    Horizontal Lines function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Button clicked'); // Check if the button click event is detected
     document.body.classList.toggle('dark');
     console.log(document.body.classList.contains('dark')); // Check if the 'dark' class is toggled correctly
+    toggleLineColors();
+    resetButtonSize();
   });
+
+  function toggleLineColors() {
+    const horizontallines = document.querySelectorAll('.horizontal-line');
+    horizontallines.forEach(line => {
+      line.classList.toggle('dark');
+    })
+  }
+
+  function resetButtonSize() {
+    themeToggleButton.computedStyleMap.transform = 'scale(1)'
+  }
 });
